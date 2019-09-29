a43d80d7e17d3388bd630eb3b27e488f50e25363
Gogo1211 <gbgodlike1211@gmail.com>
2019-09-29 16:57:53 -0400

    add username to login response

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
   def create
     sleep 3
     if (params[:username] == 'Admin' && params[:password] == 'test1A')
-      render json: { authorized: true }, status: :ok
+      render json: { authorized: true, username: params[:username] }, status: :ok
     else
       render json: { authorized: false }, status: :unauthorized
     end
